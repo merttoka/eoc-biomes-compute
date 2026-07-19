4d8cb720a7f62793c3ebdfb50992f6f7aedb2d7f
merttoka <17174049+merttoka@users.noreply.github.com>
2026-07-19 02:03:25 -0700

    sequencer: timeline+test-framework packages, core/tests asmdef scaffold

---
+++ Assets/Tests/EditMode/SmokeTest.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace Biomes.Sequencer.Tests
+{
+    public class SmokeTest
+    {
+        [Test]
+        public void TestAssemblyCompilesAndRuns() => Assert.Pass();
+    }
+}
